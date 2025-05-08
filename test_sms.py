80f2bec7ffc70268638ab7c40dfb50039d4ca478
MylesGoHam <mylescunningham0@gmail.com>
2025-05-07 18:19:10 -0700

    Update test_sms.py

--- test_sms.py
+++ test_sms.py
@@ -3,7 +3,7 @@
 from sms_utils import send_sms_alert
 
 # Replace with your test number (must be verified in Twilio if using trial)
-test_number = "2096402828"
+test_number = "+12096402828"
 test_message = "🚨 Test SMS from Zyberfy backend!"
 
 send_sms_alert(test_number, test_message)
